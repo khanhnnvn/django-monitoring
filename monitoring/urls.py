c0f0d7a5183a23d0be53f9e93ce957458ed13440
Paul Ahern <ahernp@ahernp.com>
2014-03-24 10:06:27 +0000

    Use Class-based view

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
 from django.conf.urls import patterns, url
-from .views import dashboard
+from .views import Dashboard
 
 urlpatterns = patterns('',
     url(regex=r'^dashboard/$',
-        view=dashboard,
+        view=Dashboard.as_view(),
         name='dashboard'),
 )
--- monitoring/views.py
+++ monitoring/views.py
@@ -1,68 +1,71 @@
 from __future__ import absolute_import
 
-from django.contrib.auth.decorators import login_required
+import os
+from subprocess import Popen, PIPE
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils import timezone
 from django.conf import settings
+from django.views.generic import TemplateView
+
+from braces.views import LoginRequiredMixin
 
 from .models import Log
 
+# Shell commands: Name and command
+SHELL_COMMANDS = [
+    ('hostname', 'hostname'),
+    ('gitversion', 'git log -n 1'),
+    ('postgresql_version', 'psql --version'),
+    ('python_packages', 'pip freeze'),
+]
 
-@login_required
-def dashboard(request):
+# Flags in settings: Their expected values.
+SETTINGS_FLAGS = [
+    ('DEBUG', False),
+    ('DEVELOP', False),
+]
+
+
+def run_shell_command(command, cwd):
+    """
+    Run command in shell and return results.
+    """
+    p = Popen(command, shell=True, cwd=cwd, stdout=PIPE)
+    stdout = p.communicate()[0]
+    if stdout:
+        stdout = stdout.strip()
+    return stdout
+
+
+class Dashboard(LoginRequiredMixin, TemplateView):
     """
     Build page providing information about the state of the system.
     """
-    import os
-    from subprocess import Popen, PIPE
+    template_name = 'monitoring/dashboard.html'
 
-    # Shell commands: Name and command
-    SHELL_COMMANDS = [
-        ('hostname', 'hostname'),
-        ('gitversion', 'git log -n 1'),
-        ('postgresql_version', 'psql --version'),
-        ('python_packages', 'pip freeze'),
-    ]
+    def get(self, request, *args, **kwargs):
+        context = {}
 
-    # Flags in settings: Their expected  and actual values.
-    SETTINGS_FLAGS = [
-        ('DEBUG', False),
-        ('DEVELOP', False),
-    ]
+        # Versions
+        cwd = settings.DJANGO_ROOT
+        for name, shell_command in SHELL_COMMANDS:
+            context[name] = run_shell_command(shell_command, cwd)
 
-    def run_shell_command(command, cwd):
-        """
-        Run command in shell and return results.
-        """
-        p = Popen(command, shell=True, cwd=cwd, stdout=PIPE)
-        stdout = p.communicate()[0]
-        if stdout:
-            stdout = stdout.strip()
-        return stdout
+        # Settings Flags
+        context['settings_flags'] = []
+        for name, expected in SETTINGS_FLAGS:
+            actual_setting = getattr(settings, name, None)
+            context['settings_flags'].append({
+                'name': name,
+                'unexpected': expected != actual_setting,
+                'actual': actual_setting
+            })
 
-    context = {}
+        context['error_msgs'] = Log.objects.filter(level='ERROR')[:2]
+        context['warning_msgs'] = Log.objects.filter(level='WARNING')[:2]
+        context['info_msgs'] = Log.objects.filter(level='INFO')[:2]
 
-    # Versions
-    cwd = settings.DJANGO_ROOT
-    for name, shell_command in SHELL_COMMANDS:
-        context[name] = run_shell_command(shell_command, cwd)
-
-    # Settings Flags
-    context['settings_flags'] = []
-    for name, expected in SETTINGS_FLAGS:
-        actual_setting = getattr(settings, name, None)
-        context['settings_flags'].append({
-            'name': name,
-            'unexpected': expected != actual_setting,
-            'actual': actual_setting
-        })
-
-    context['error_msgs'] = Log.objects.filter(level='ERROR')[:2]
-    context['warning_msgs'] = Log.objects.filter(level='WARNING')[:2]
-    context['info_msgs'] = Log.objects.filter(level='INFO')[:2]
-
-    context['time_checked'] = timezone.now()
-    return render_to_response('monitoring/dashboard.html',
-                              context,
-                              RequestContext(request))
+        context['time_checked'] = timezone.now()
+        return self.render_to_response(context)
